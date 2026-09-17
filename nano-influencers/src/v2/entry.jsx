0e08a7f36ae509ba20bd110f309de604e5b84b37
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-17 19:04:13 +0100

    Load dashboard fidelity fixes

--- nano-influencers/src/v2/entry.jsx
+++ nano-influencers/src/v2/entry.jsx
@@ -6,6 +6,7 @@ import V2Register from "./pages/V2Register.jsx";
 import V2Login from "./pages/V2Login.jsx";
 import V2ForgotPassword from "./pages/V2ForgotPassword.jsx";
 import V2Dashboard from "./pages/V2Dashboard.jsx";
+import "./pages/v2-dashboard-fixes.css";
 import {
   EmailVerificationPage,
   WhatsAppNumberPage,
