f8128ff9a67a3317279dd6a8d0d74863c30413ba
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-17 17:42:18 +0100

    Route V2 login and forgot password screens

--- nano-influencers/src/v2/entry.jsx
+++ nano-influencers/src/v2/entry.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import V2Landing from "./pages/V2Landing.jsx";
 import V2Register from "./pages/V2Register.jsx";
+import V2Login from "./pages/V2Login.jsx";
+import V2ForgotPassword from "./pages/V2ForgotPassword.jsx";
 
 const pathname = window.location.pathname;
 const root = document.getElementById("root");
 
+function renderV2(component) {
+  createRoot(root).render(
+    <React.StrictMode>
+      <BrowserRouter>{component}</BrowserRouter>
+    </React.StrictMode>,
+  );
+}
+
 if (pathname === "/" || pathname === "") {
-  createRoot(root).render(
-    <React.StrictMode>
-      <V2Landing />
-    </React.StrictMode>,
-  );
+  renderV2(<V2Landing />);
 } else if (pathname === "/register") {
-  createRoot(root).render(
-    <React.StrictMode>
-      <V2Register />
-    </React.StrictMode>,
-  );
+  renderV2(<V2Register />);
+} else if (pathname === "/login") {
+  renderV2(<V2Login />);
+} else if (pathname === "/forgot-password") {
+  renderV2(<V2ForgotPassword />);
 } else {
   // Keep all other authenticated/auth routes on the current implementation
   // while V2 is rebuilt screen-by-screen.
